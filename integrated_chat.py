d84ed9aeb0c80e3eecadced2c51026fd2b697d4e
Wrathotw <dawid.maciejewski0609@gmail.com>
2025-05-05 14:11:17 +0200

    Add files via upload

---
+++ integrated_chat.py
@@ -0,0 +1,58 @@
+import streamlit as st
+import random
+import time
+import os
+import asyncio
+
+from dotenv import find_dotenv, load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_mistralai.chat_models import ChatMistralAI
+from langchain_core.prompts import ChatPromptTemplate
+
+dotenv_path = find_dotenv()
+load_dotenv(dotenv_path)
+
+API_KEY = os.getenv("API_KEY")
+chat = ChatMistralAI(api_key=API_KEY)
+
+# Streamed response emulator
+def response_generator(joke_topic):
+    prompt = ChatPromptTemplate.from_template("Tell me a joke about {topic}")
+    chain = prompt | chat
+
+    response = chain.invoke({"topic": joke_topic}).content
+
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.1)
+
+st.title("My first chat app")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# Accept user input
+if prompt := st.chat_input("Enter a joke topic:"):
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    # Display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)   
+
+    # Stream assistant response
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        response_stream = response_generator(prompt)
+
+        for word in response_generator(prompt):
+            full_response += word
+            message_placeholder.markdown(full_response)
+
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
